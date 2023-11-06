90640227c5e78fbe5d12c41029eae44d9800fa68
Tom PRIETO <tom.prieto@etu.unistra.fr>
2023-11-06 10:24:11 +0100

    ajout de la partie 1

---
+++ partie1.s
@@ -0,0 +1,24 @@
+li a0, 0
+
+boucle:
+    # Affiche la valeur dans a0
+    mv a0,t1
+    li a7, 1   # Appel système pour afficher
+    ecall
+
+    # Attente de 500 ms (0.5 seconde)
+    li a7, 32  # Appel système pour sleep
+    mv t1, a0
+    li a0, 500  # 500 millisecondes (0.5 seconde)
+    ecall
+
+    # Incrémente la valeur
+    addi t1, t1, 1
+
+    # Vérifie si nous avons atteint 10
+    li t0, 11
+    bne t1, t0, boucle
+
+    # Terminaison du programme
+    li a7, 10  # Appel système pour exit
+    ecall
